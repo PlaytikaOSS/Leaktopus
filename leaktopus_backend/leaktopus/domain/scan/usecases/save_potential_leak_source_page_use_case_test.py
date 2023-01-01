2355956a8eb6c1b5edf09cbff183d167d33bec15
rotemr <rotemr@playtika.com>
2023-01-01 12:27:29 +0200

    Add additional test to cover the case of multiple IOLs per leak.

--- leaktopus_backend/leaktopus/domain/scan/usecases/save_potential_leak_source_page_use_case_test.py
+++ leaktopus_backend/leaktopus/domain/scan/usecases/save_potential_leak_source_page_use_case_test.py
@@ -49,6 +49,43 @@ def test_should_save_potential_leak_source_page_successfully(
     leak_provider_mock.add_leak.assert_called()
 
 
+def test_should_save_potential_leak_source_page_with_multiple_iols_support(
+    factory_leak_service,
+    ignore_pattern_provider_mock,
+    potential_leak_source_filter_mock,
+    page_results,
+    potential_leak_source_scan_status_provider_mock,
+):
+    search_query = "test"
+    scan_id = 1
+    current_page_number = 1
+
+    # leak_provider_mock.add_leak.return_value = None
+    ignore_pattern_provider_mock.get_ignore_patterns.return_value = []
+    potential_leak_source_filter_mock.filter.return_value = True
+    potential_leak_source_scan_status_provider_mock.mark_as_analyzing.return_value = (
+        None
+    )
+
+    leak_service = factory_leak_service()
+    use_case = SavePotentialLeakSourcePageUseCase(
+        leak_service=leak_service,
+        potential_leak_source_filter=potential_leak_source_filter_mock,
+        email_extractor=EmailExtractor(organization_domains=["test.com", "test2.com"]),
+        potential_leak_source_scan_status_service=PotentialLeakSourceScanStatusService(
+            provider=potential_leak_source_scan_status_provider_mock
+        ),
+    )
+    use_case.execute(scan_id, page_results, search_query, current_page_number)
+    saved_leaks = leak_service.get_leaks()
+
+    # Assert two leaks were saved.
+    assert len(saved_leaks) == 2
+
+    # Assert the first leak has two IOLs.
+    assert len(saved_leaks[0].IOL) == 2
+
+
 @pytest.fixture
 def potential_leak_source_scan_status_provider_mock(mocker):
     return mocker.patch.object(
@@ -70,11 +107,11 @@ def potential_leak_source_filter_mock(mocker):
 def page_results():
     return [
         PotentialLeakSource(
-            url="url_1",
-            name="name_1",
-            html_url="html_url_1",
+            url="url_repo_1",
+            name="name_iol_1",
+            html_url="html_url_iol_1",
             last_modified="Sat, 19 Dec 2022 15:34:56 UTC",
-            content="contnet_1",
+            content="content_iol_1",
             repo_name="repo_name_1",
             repo_description="repo_description_1",
             context={
@@ -82,12 +119,13 @@ def page_results():
             },
             source="source_1",
         ),
+        # Same IOL and repo as the first.
         PotentialLeakSource(
-            url="url_1",
-            name="name_1",
-            html_url="html_url_1",
+            url="url_repo_1",
+            name="name_iol_1",
+            html_url="html_url_iol_1",
             last_modified="Sat, 19 Dec 2022 15:34:56 UTC",
-            content="contnet_1",
+            content="content_iol_1",
             repo_name="repo_name_1",
             repo_description="repo_description_1",
             context={
@@ -95,12 +133,27 @@ def page_results():
             },
             source="source_1",
         ),
+        # Additional IOL of repo 1
         PotentialLeakSource(
-            url="url_2",
-            name="name_2",
-            html_url="html_url_2",
+            url="url_repo_1",
+            name="name_iol_2",
+            html_url="html_url_iol_2",
             last_modified="Sat, 19 Dec 2022 15:34:56 UTC",
-            content="contnet_2",
+            content="content_iol_2",
+            repo_name="repo_name_1",
+            repo_description="repo_description_1",
+            context={
+                "file_name": "file_name_1",
+            },
+            source="source_1",
+        ),
+        # Additional IOL in another repo (2).
+        PotentialLeakSource(
+            url="url_repo_2",
+            name="name_iol_3",
+            html_url="html_url_iol_2",
+            last_modified="Sat, 19 Dec 2022 15:34:56 UTC",
+            content="content_iol_3",
             repo_name="repo_name_2",
             repo_description="repo_description_2",
             context={
