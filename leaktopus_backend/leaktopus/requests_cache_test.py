1ba5d75b28f11c2b27af0ecb8d5e1d05cbcd73b6
Shaked Klein Orbach <klein.shaked@gmail.com>
2023-02-11 19:07:47 +0200

    fix: fix unit tests for requests cache

--- leaktopus_backend/leaktopus/requests_cache_test.py
+++ leaktopus_backend/leaktopus/requests_cache_test.py
@@ -1,32 +1,13 @@
-from random import randint
-
 import requests
-from flask import url_for, Blueprint
 from leaktopus.requests_cache import CustomCachedSessionWithPickleSupport
-from pytest_httpserver import HTTPServer
 from requests_cache import BaseCache
 from werkzeug import Request, Response
 
 
-def testRequestsCacheWithCacheSuccess(app,httpserver):
+def testRequestsCacheWithCacheSuccess(app, httpserver):
     config = app.config
-    config['REQUESTS_CACHE_ENABLED'] = True
-    config['REQUESTS_CACHE_BACKEND'] = BaseCache()
-    CustomCachedSessionWithPickleSupport.register(app)
+    config['REQUESTS_CACHE_ENABLED'] = False
 
-    counter = 0
-
-    def handler(request: Request):
-        nonlocal counter
-        counter += 1
-        return Response("Hello, World!", status=200)
-
-    httpserver.expect_request("/test").respond_with_handler(handler)
-    assert requests.get(httpserver.url_for("/test")).status_code == 200
-    assert requests.get(httpserver.url_for("/test")).text == "Hello, World!"
-    assert counter == 1
-
-def testRequestsCacheWithoutCacheSuccess(app,httpserver):
     counter = 0
 
     def handler(request: Request):
@@ -38,3 +19,10 @@ def testRequestsCacheWithoutCacheSuccess(app,httpserver):
     assert requests.get(httpserver.url_for("/test")).status_code == 200
     assert requests.get(httpserver.url_for("/test")).text == "Hello, World!"
     assert counter == 2
+
+    config['REQUESTS_CACHE_ENABLED'] = True
+    config['REQUESTS_CACHE_BACKEND'] = BaseCache()
+    CustomCachedSessionWithPickleSupport.register(app)
+    assert requests.get(httpserver.url_for("/test")).status_code == 200
+    assert requests.get(httpserver.url_for("/test")).text == "Hello, World!"
+    assert counter == 3
